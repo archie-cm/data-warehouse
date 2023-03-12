e70579c4da41efcf29e3863e2be07e6fb0e5d5e4
Archie Citra Muhammad <108534539+archie-cm@users.noreply.github.com>
2023-03-12 09:55:30 +0700

    Add files via upload

--- query.sql
+++ query.sql
@@ -1,11 +1,27 @@
-with Flattened as (
-  SELECT DISTINCT channelGrouping, date, geoNetwork_country,
-  COUNT(DISTINCT CONCAT(fullVisitorId, CAST(visitId AS STRING))) AS Transactions
-  FROM `data-to-insights.ecommerce.rev_transactions`
-  GROUP BY 1, 2, 3
-  ORDER BY date, geoNetwork_country ASC
-)
-
-select channelGrouping as Channel, ARRAY_AGG(date) as Date, ARRAY_AGG(geoNetwork_country) as Country, ARRAY_AGG(Transactions) as Transactions, from Flattened
-  GROUP BY channelGrouping
-limit 10
+WITH
+  cte AS (
+  SELECT
+    DISTINCT channelGrouping,
+    date,
+    geoNetwork_country,
+    COUNT(DISTINCT CONCAT(fullVisitorId, CAST(visitId AS STRING))) AS Transactions,
+    SUM(totals_totalTransactionRevenue) AS Total_Revenue
+  FROM
+    `data-to-insights.ecommerce.rev_transactions`
+  GROUP BY
+    1,
+    2,
+    3
+  ORDER BY
+    date,
+    geoNetwork_country ASC )
+SELECT
+  channelGrouping AS Channel,
+  ARRAY_AGG(date) AS Date,
+  ARRAY_AGG(geoNetwork_country) AS Country,
+  ARRAY_AGG(Transactions) AS Transactions,
+  ARRAY_AGG(Total_Revenue) AS Total_Revenue
+FROM
+  cte
+GROUP BY
+  channelGrouping
